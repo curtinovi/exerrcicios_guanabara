2a80b223a41019dcc8850d02ade36fa5bdf54fb7
curtinovi <106404287+curtinovi@users.noreply.github.com>
2022-05-27 15:55:47 -0300

    Exercícios Guanabara

---
+++ ex001.py
@@ -0,0 +1,5 @@
+nome=input('Qual o seu nome?')
+idade=input('Qual a sua idade?')
+nascimento=input('Qual sua data de nascimento?')
+print('Meu nome é', nome, 'e tenho', idade, 'anos. Nasci no dia', nascimento, '.')
+
---
+++ ex003.py
@@ -0,0 +1,6 @@
+primeiro=input('Primeiro número: ')
+segundo=input('Segundo número: ')
+primeiro=int(primeiro)
+segundo=int(segundo)
+soma=primeiro*segundo
+print(soma)
